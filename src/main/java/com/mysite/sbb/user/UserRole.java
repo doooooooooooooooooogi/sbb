c891e8430e651d5f815b8789cbbf883ebf0f44fa
dk88 <121776909+doooooooooooooooooogi@users.noreply.github.com>
2023-06-29 11:25:53 +0900

    initial commit

---
+++ src/main/java/com/mysite/sbb/user/UserRole.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb.user;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+	// 관리자(ROLE_ADMIN) 또는 유저 (ROLE_USER)
+	ADMIN("ROLE_ADMIN"),
+	USER("ROLE_USER");
+	
+	private String value;
+	
+	UserRole(String value) {
+		this.value = value;
+	}
+}
